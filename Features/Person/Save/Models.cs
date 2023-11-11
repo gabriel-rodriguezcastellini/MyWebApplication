90bc52ff679ce9cf8afaadda15d92beb742699b2
Gabriel Rodriguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2023-11-11 16:11:17 -0300

    Domain Entity Mapping

---
+++ Features/Person/Save/Models.cs
@@ -0,0 +1,25 @@
+﻿namespace MyWebApplication.Features.Person.Save
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string BirthDay { get; set; } = null!;
+    }
+
+    public class Response
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+        public int Age { get; set; }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
---
+++ Features/Person/Save/SavePerson.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace MyWebApplication.Features.Person.Save
+{
+    public class SavePerson : EndpointWithMapping<Request, Response, Person>
+    {
+        public override void Configure()
+        {
+            Put("/person");
+        }
+
+        public override Task HandleAsync(Request r, CancellationToken ct)
+        {
+            Person entity = MapToEntity(r);
+            Response = MapFromEntity(entity);
+            return SendAsync(Response, cancellation: ct);
+        }
+
+        public override Person MapToEntity(Request r)
+        {
+            return new()
+            {
+                Id = r.Id,
+                DateOfBirth = DateOnly.Parse(r.BirthDay, new CultureInfo("en-US")),
+                FullName = $"{r.FirstName} {r.LastName}"
+            };
+        }
+
+        public override Response MapFromEntity(Person e)
+        {
+            return new()
+            {
+                Id = e.Id,
+                FullName = e.FullName,
+                UserName = $"USR{e.Id:0000000000}",
+                Age = (DateOnly.FromDateTime(DateTime.UtcNow).DayNumber - e.DateOfBirth.DayNumber) / 365,
+            };
+        }
+    }
+}
